61f14a8d5cca38b110f70e402170c7226005df29
elenaterehova <tterekhovaa@mail.ru>
2024-03-09 12:57:44 +0300

    change mark to skip in first method, add 3rd method with xfail

--- test_fixture_with_mark.py
+++ test_fixture_with_mark.py
@@ -15,7 +15,7 @@ def browser():
 
 class TestMainPage1():
 
-    @pytest.mark.smoke
+    @pytest.mark.skip
     def test_guest_should_see_login_link(self, browser):
         browser.get(link)
         browser.find_element(By.CSS_SELECTOR, "#login_link")
@@ -25,3 +25,8 @@ class TestMainPage1():
     def test_guest_should_see_basket_link_on_the_main_page(self, browser):
         browser.get(link)
         browser.find_element(By.CSS_SELECTOR, ".basket-mini .btn-group > a")
+
+    @pytest.mark.xfail(reason="fixing this bug right now")
+    def test_guest_should_see_search_button_on_the_main_page(self, browser):
+        browser.get(link)
+        browser.find_element(By.CSS_SELECTOR, "button.favorite")
